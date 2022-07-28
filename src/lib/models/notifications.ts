71f816df0969cfaad1117d0c0b46d640736b44c6
Randell <piszmogcode@gmail.com>
2022-07-27 20:18:35 -0600

    Make fields optional

--- src/lib/models/notifications.ts
+++ src/lib/models/notifications.ts
@@ -1,11 +1,11 @@
 export interface Notification {
   id: string;
   title: string;
-  url: string;
+  url?: string;
   reason: string;
   unread: boolean;
   updated_at: string;
-  last_read_at: string;
+  last_read_at?: string;
   notification_type: string;
   repository: Repository;
 }
