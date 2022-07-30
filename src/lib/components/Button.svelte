4b6080c399233347964b330703c5b50e963de58d
Randell <piszmogcode@gmail.com>
2022-07-30 13:41:01 -0600

    Add generic button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  let className: string = '';
+  export { className as class };
+
+  const dispatch = createEventDispatcher();
+
+  const click = () => {
+    dispatch('click');
+  };
+</script>
+
+<button on:click={click} class={className}>
+  <slot />
+</button>
